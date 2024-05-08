d659199f5fcba3f951b4f4d4b54c4a3ebe1813e8
pixcelo <bodomu@gmail.com>
2024-05-09 07:13:24 +0900

    add: プロジェクト追加

---
+++ Insight/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Insight.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
