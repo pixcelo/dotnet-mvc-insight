27d4d2d9bc1057a55866f1647e08b94f1d004db2
pixcelo <bodomu@gmail.com>
2024-05-24 03:09:15 +0900

    update: API修正

--- Insight/ApiControllers/InsightController.cs
+++ Insight/ApiControllers/InsightController.cs
@@ -2,19 +2,22 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Insight.Controllers
+namespace Insight.ApiControllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class InsightController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
         private readonly HttpClient _httpClient;        
         private readonly IAccessTokenAccessor _accessTokenAccessor;
 
         public InsightController(
+            IConfiguration configuration,
             IHttpClientFactory httpClientFactory, 
             IAccessTokenAccessor accessTokenAccessor)
         {
+            _configuration = configuration;
             _httpClient = httpClientFactory.CreateClient();            
             _accessTokenAccessor = accessTokenAccessor;
         }
@@ -23,15 +26,20 @@ namespace Insight.Controllers
         public async Task<string> GetInsight()
         {
             try
-            {                
+            {
                 var accessToken = _accessTokenAccessor.GetAccessToken();
+                //var accessToken = _configuration.GetSection("Authentication:Facebook:AccessToken").Value;
 
                 if (string.IsNullOrEmpty(accessToken))
                 {
                     throw new ArgumentNullException("Access token is not set.");
                 }
-                
-                var requestUri = "";
+
+                var base_url = "https://graph.facebook.com";
+                var id = "";
+                var version = "v19.0";
+                var endpoint = "insights?metric=impressions,reach,profile_views&period=day";
+                var requestUri = $@"{base_url}/{version}/{id}/{endpoint}&access_token={accessToken}";
 
                 var response = await _httpClient.GetAsync(requestUri);
                 response.EnsureSuccessStatusCode();
